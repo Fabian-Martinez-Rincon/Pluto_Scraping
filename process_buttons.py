f5db0f2e52aa122cde357df7b599b5300f497ab8
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-08-14 22:21:21 -0300

    Empezando el proceso de scroll en paralelo

--- process_buttons.py
+++ process_buttons.py
@@ -1,35 +1,59 @@
-import json
-import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-import os
-import io
+from selenium.webdriver.common.keys import Keys
+import json
+import concurrent.futures
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 options = Options()
+options = Options()
 options.headless = True
+options.add_argument("--start-maximized")  # Inicia maximizado
 
-html_dir = 'html_files'
-os.makedirs(html_dir, exist_ok=True)
+def scroll(driver):
+    elementos= WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, 'div.custom-scroll > ul.reset-ul')
+            )
+        )
+    elementos.click()
+    body_element = driver.find_element(By.TAG_NAME, 'body')
+    for i in range(40):
+        body_element.send_keys(Keys.PAGE_DOWN)
+        if (i == 15):
+            time.sleep(2)
+        if (i == 30):
+            time.sleep(2)
 
 def procesar_link(data):
     text, url = data['text'], data['href']
+    print(f"Procesando {text}")
+    print(f"URL: {url}")
     driver = webdriver.Chrome(options=options)
     try:
         driver.get(url)
-        time.sleep(5)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        scroll(driver)
         time.sleep(2)
-        html_content = driver.page_source
-        filename = os.path.join(html_dir, f"{text.replace(' ', '_').replace('/', '_')}.html")
-        with io.open(filename, 'w', encoding='utf-8') as f:
-            f.write(html_content)
-        print(f"HTML de {text} guardado en {filename}")
+        items = driver.find_elements(By.CSS_SELECTOR, "div.custom-scroll > ul.reset-ul > li.itemContainer.vod-item-poster-atc")
+        alt_attributes = []
+        time.sleep(2)
+        for item in items:
+            try:
+                img_element = item.find_element(By.TAG_NAME, 'img')
+                alt = img_element.get_attribute('alt')
+                alt_attributes.append(alt)
+            except NoSuchElementException:
+                alt_attributes.append("No image available")
+        print(alt_attributes)
     finally:
         driver.quit()
 
 with open('buttons.json', 'r') as file:
     links_json = json.load(file)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
     executor.map(procesar_link, links_json)
